c24e83de728a7a707d30b42631f5e8dff6bae367
chaos29092 <wo94yun>
2014-12-08 17:17:39 +0800

    english add

---
+++ app/lang/en/page.php
@@ -0,0 +1,188 @@
+<?php
+
+return array(
+//menu
+    'home' => 'Index',
+    'products' => 'Product',
+    'aboutUs' => 'About NUCLCON',
+    'customerVisit' => 'Customer Visit',
+    'service' => 'Service',
+    'merchants' => 'Agent Wanted',
+    'contact' => 'Contact',
+
+//column
+    'sideTitle' => 'Product List',
+    'allProduct' => 'All Product',
+    'mbh' => 'mobile boat hoist',
+    'needHelp' => 'What can I do for you？',
+    'chatNow' => 'chat now',
+    'chatOnline' => 'chat online',
+    'orMail' => 'or email to us',
+
+//product page
+    'sendMailToUs' => 'send us an email',
+    'getPrice' => 'inquiry',
+    'dec' => 'product details',
+    'parameters' => 'Specifications',
+    'fillInquiry' => 'fill in your inquiry',
+    'email' => 'Email',
+    'name' => 'Name',
+    'message' => 'Message',
+    'send' => 'send',
+
+//price list page
+    'smbh' => 'small range',
+    'mmbh' => 'middle range',
+    'bmbh' => 'large range',
+
+//about us
+    'nucleonCompany' => 'NUCLEON (Henan) Mobile Boat Hoist Co., Ltd',
+    'about_p1' => 'we are specialized in manufacturing mobile boat hoist，and we are 
+located in Henan Crane Industrial Park, which has good raputition by "Town of China Crane" 
+which is in Changyuan County of China.Our capital is USD4000,000，and land area is 320,000
+㎡. There are 1700 staff in our company and 400 people of them are sebior and medium 
+technicians',
+    'about_p2' => 'Our company was already awarded ISO9001 Certificate of Quality 
+System,ISO14001 Environmental Management System andOHSAS18001 Occupation Health and Safety 
+Management System Certification。In 2010，company is awarded by "Advanced enterprise By se 
+of foreign capital"; In 2011，be awarded by "AAA Grade Quality Enterprise in Henan 
+Province"；In 2012，be awarded by "National High and New Technology Enterprise"，“Quality 
+Credit Product”and“TOP500 National Credit Enterprise”.Our brad of NUCLEON“overhead 
+crane and gantry crane”and“Steel wire hoist”are awarded“Famous brand in Henan Province
+”；In 2013，our company is awarded“National outstanding enterprise by quality credit”and
+“National stable quality inspection product”，and brand “NUCLEON”is regarded as "Famous 
+Trademark in Henan Province"',
+    'about_p3' => 'There are 1600 sets advanced international and domestic equipments 
+including manufacturing and inspecting equipments. Also, we have U shape chute pull 
+equipments and biggest 100t hoist test platform in China,Plasma cutting machine, CNC lathe, 
+vertical Machining center, CNC floor type boring and milling machines, heavy guns Longmen 
+welder, which become solid basis for manufacturing high quality produt.',
+    'about_p4' => 'Our company is specialized in design and manufacture mobile boat hoist
+and other crane equipments. Our anual yield of 2013 is 260 million dollars, which is 
+advanced in China',
+    'about_p5' => 'Our manufacture area is 214000㎡ and there are 23600㎡ area including 
+modern office building, multi-functional hall and staff apartment, which is a modern 
+enterprise integrated in research,manufacture and sales.',
+    'factoryTour' => 'factory tour',
+    'ff1' => 'NUCLEON（Henan）Mobile Boat Hoist Co.,Ltd',
+    'f1' => 'Lion with fish tail',
+    'f2' => 'Full image of NUCLEON administration building and east gate',
+    'f3' => 'Full image of factory area',
+    'f4' => 'night sight of office building',
+    'f5' => '#2 Apartment',
+    'f6' => 'Open air working site',
+    'f7' => 'multi-functional hall',
+    'f8' => 'training center',
+    'f9' => 'Corner of factory',
+
+    'researchteam' => 'research team',
+    'y1' => 'Operating brand of NUCLEON，not only brand spread, but also brand management, 
+creat and maintain',
+    'r1' => 'Our company was established in professional and technical backbone as the main 
+research lab, the application of high-tech, high-tech technology to design software Fruit, 
+development of market demand for products, to meet the market differentiation needs, and 
+try a new breakthrough in the aspects of environmental protection, security, intelligent 
+and to walk in the forefront of the industry.',
+    'r2' => 'Our company technical power is composed by 5 parts: chief engineer office, 
+electrical design department, light design department, design department overhead crane and 
+gantry crane and workmanship department. The technical team composed by senior technical 
+staff, is the dominant force by product R & D and  meeting personal demand, and keen to the 
+world advanced technology is goal of product development.',
+    'r3' => 'At the beginning, we are committed to high quality product manufacturing, 
+HC100 seiers steel wire rope electric hoist independent researched and manufactured by our 
+company is ahead of same field in our country by quality and performance. European STHS 
+electric hoist double girder crane, NLX European electric suspension, 320 ton MG universal 
+QD type gantry crane, QD320 tons of general overhead crane,  YZ type casting 400m high gate 
+crane and the European STi electric hoist that its applied to
+low building height , HD of European type of electric single beam crane and other equipment 
+manufacturing shows our research and development ability has reached the leading domestic 
+level.',
+
+    'productionEquipment' => 'manufacture equipment',
+    'pe' => 'Our company is equiped advanced facilities in the industry, has the largest 
+national 100 ton test platform , testing center and matching machining equipment, such as 
+CNC surface drilling machine, CNC flame plasma cutting machine, CNC lathe, NC boring, 
+nondestructive testing instrument
+. Advanced equipment becomes solid basis for manufacturing high-quality products.',
+    'pe_1' => 'Beijing Polonus automatic painting line',
+    'pe_2' => 'Japan CY-K40 full automatic CNC lathe',
+    'pe_3' => 'Germany Messer CNC flame plasma cutting machine',
+    'pe_4' => 'Qingdao Double Star steel plate shot blasting line',
+//home
+
+//customer visit
+    'customerVisit' => 'customer visit',
+
+//product details
+    'pdec_1' => 'Our mobile boat hoist is designed as per requirements of customer and used
+for carry all kinds of boats and rated lifting capacity can reach',
+    'pdec_2' => '。For design,we fully consider height of top beam possibly beyond by 
+lifting object. So we choose "C" structure for object lifted easily go into mobile boat 
+hoist',
+    'pdec_3' => 'Yacht crane，which is also called boat hoist，is widely used water sports.
+club, sailing and voyage learning',
+    'pdec_4' => 'Mobile boat hoist,which can carry boat with different capacity (10t-1000t)
+out of water,is used for doing maintainenance for boat or make new boat into water.',
+    'pdec_50t' => 'max.width of lifting boat is 6m。',
+    'pdec_100t' => 'max.width of lifting boat is 8.5m。',
+    'pdec_150t' => 'max.width of lifting boat is 9.5m。',
+    'pdec_200t' => 'max.width of lifting boat is 10m。',
+    'pdec_300t' => 'max.width of lifting boat is 12m。',
+    'pdec_400t' => 'max.width of lifting boat is 12m。',
+    'pdec_500t' => 'max.width of lifting boat is 15m。',
+    'pdec_700t' => 'max.width of lifting boat is 16m。',
+    'pdec_800t' => 'max.width of lifting boat is 18m。',
+    'pdec_1000t' => 'max.width of lifting boat is 18m。',
+
+    'summary' => 'summary',
+    'summary_1' => 'Mobile boat hoist can put boat on the bank, so it is easy to do 
+maintenance and repair for boats. This can greatly reduce the maintenance cost.',
+    'summary_2' => 'The mobile boat hoist comprises a main structure, drive system, 
+steering system, lifting system, walking system, engine
+, electric control and safety protection system, wireless remote control or cab (optional) 
+etc.',
+
+    'mainTechnicalFeatures' => 'main technical features',
+    'mtf_1' => 'Composed of main structure,wheel system,lifting system,steering
+system,hydraulic tranmission system and electrical system.Main structure is of “匚”, 
+which can carry boat which self height is over height of boat hoist.',
+    'mtf_2' => 'Lifting, running system all use electric hydraulic driving system. 
+Hydraulic transmission with smooth motion transmission , quick response
+high speed start, small inertia, braking and reversing; has the advantages of small volume, 
+light weight, compact structure; simple operation, convenient, easy operation.In the 
+operation, can conveniently realize the stepless speed regulation; easy to achieve overload 
+protection; pressure element to realize standardization, seriation,universalization,with 
+advantages of easy design, manufacture and application.',
+    'mtf_3' => 'Hoisting mechanism adopts multi-point lifting, can effectively disperse the 
+forces acting on the hull. Each lifting point can be movable adjustment to meet the 
+different needs of the hull.',
+    'mtf_4' => 'Using a soft but strong lifting belt, which are well-distributed on the 
+surface of ship, can lift boat, yacht or wooden boat, but has no damage to the surface.',
+    'mtf_5' => 'It can quickly make ships to be arranged together, and distance between
+boats can be smaller, then to save more storage space..
+Room.',
+
+    'technicalCondition' => 'Design Conditions',
+    'tc_1' => 'Design, structure and mechanism caculation all meet European standard of
+FEM1001/98。',
+
+    'environmentalConditions' => 'Environment Conditions',
+
+//specification table
+    'pt1' => 'rated lift capacity',
+    'pt2' => 'lifting speed（load/unload）',
+    'pt3' => 'walking speed（load）',
+    'pt4' => 'max.climbing slope（load）',
+    'pt5' => 'diesel engine',
+    'pt6' => 'lifting system/lifting position',
+    'pt7' => 'lifting belf length/lifting capacity for sigle belt',
+    'pt8' => 'tyre（quantity/specification）',
+    'pt9' => 'ground pressure',
+    'pt10' => 'steering radius (pivot steering)',
+    'pt11' => 'steering radius (Ackerman steering)',
+
+//page title
+    'indexTitle' => 'professional designer and manufacturer of mobile boat hoist',
+
+
+);
