93a62dfdb615a7c93130cd1dfb588e6a4692a0be
chaos29092 <chaos29092@gmail.com>
2015-01-12 22:29:05 +0800

    fv done

--- app/views/page/factory_views.blade.php
+++ app/views/page/factory_views.blade.php
@@ -1,11 +1,11 @@
 @extends('sidebar_master')
 
 @section('title')
-nucleon - {{Lang::get('page.factoryViews')}}
+    nucleon - {{Lang::get('page.factoryViews')}}
 @stop
 
 @section('description')
-nucleon - {{Lang::get('page.factoryViews')}}
+    nucleon - {{Lang::get('page.factoryViews')}}
 @stop
 
 @section('topimg')
@@ -17,21 +17,88 @@ nucleon - {{Lang::get('page.factoryViews')}}
         <h4>{{Lang::get('page.factoryViews')}}</h4>
         <hr/>
         <div class="row">
-                  <div class="col-xs-6 col-md-4">
-                    <a href="{{asset('img/cv/cv_1.jpg')}}" class="thumbnail" data-lightbox="image-1" data-title="My caption">
-                      <img src="{{asset('img/cv/cv_1.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
-                    </a>
-                  </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/cv/cv_1.jpg')}}" class="thumbnail" data-lightbox="image-1" data-title="My caption">
-                    <img src="{{asset('img/cv/cv_1.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                <a href="{{asset('img/fv/factory_views_1.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 1">
+                    <img src="{{asset('img/fv/factory_views_1_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
                 </a>
             </div>
-                  <div class="col-xs-6 col-md-4">
-                    <a class="thumbnail">
-                      <img src="{{asset('img/cv/cv_3.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
-                    </a>
-                  </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_2.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 2">
+                    <img src="{{asset('img/fv/factory_views_2_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_3.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 3">
+                    <img src="{{asset('img/fv/factory_views_3_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_4.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 4">
+                    <img src="{{asset('img/fv/factory_views_4_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_5.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 5">
+                    <img src="{{asset('img/fv/factory_views_5_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_6.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 6">
+                    <img src="{{asset('img/fv/factory_views_6_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_7.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 7">
+                    <img src="{{asset('img/fv/factory_views_7_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_8.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 8">
+                    <img src="{{asset('img/fv/factory_views_8_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_9.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 9">
+                    <img src="{{asset('img/fv/factory_views_9_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_10.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 10">
+                    <img src="{{asset('img/fv/factory_views_10_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_11.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 11">
+                    <img src="{{asset('img/fv/factory_views_11_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/fv/factory_views_12.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.factoryViews')}} 12">
+                    <img src="{{asset('img/fv/factory_views_12_thum.jpg')}}" alt="{{Lang::get('page.factoryViews')}}">
+                </a>
+            </div>
+
         </div>
         <hr/>
     </div>
