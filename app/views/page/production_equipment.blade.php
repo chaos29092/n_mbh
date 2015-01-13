b5d23a6a5f14748b4a9168da0e35e96c08e938ae
chaos29092 <wo94yun>
2015-01-13 10:52:38 +0800

    eq done

--- app/views/page/production_equipment.blade.php
+++ app/views/page/production_equipment.blade.php
@@ -18,87 +18,183 @@
         <hr/>
         <div class="row">
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_1.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 1">
-                    <img src="{{asset('img/fv/factory_views_1_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_1.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_1')}}">
+                    <img src="{{asset('img/eq/eq_1_thum.jpg')}}" alt="{{Lang::get('page.eq_1')}}">
                 </a>
             </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_2.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 2">
-                    <img src="{{asset('img/fv/factory_views_2_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_2.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_2')}}">
+                    <img src="{{asset('img/eq/eq_2_thum.jpg')}}" alt="{{Lang::get('page.eq_2')}}">
                 </a>
             </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_3.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 3">
-                    <img src="{{asset('img/fv/factory_views_3_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_3.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_3')}}">
+                    <img src="{{asset('img/eq/eq_3_thum.jpg')}}" alt="{{Lang::get('page.eq_3')}}">
                 </a>
             </div>
-
         </div>
         <div class="row">
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_4.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 4">
-                    <img src="{{asset('img/fv/factory_views_4_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_4.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_4')}}">
+                    <img src="{{asset('img/eq/eq_4_thum.jpg')}}" alt="{{Lang::get('page.eq_4')}}">
                 </a>
             </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_5.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 5">
-                    <img src="{{asset('img/fv/factory_views_5_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_5.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_5')}}">
+                    <img src="{{asset('img/eq/eq_5_thum.jpg')}}" alt="{{Lang::get('page.eq_5')}}">
                 </a>
             </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_6.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 6">
-                    <img src="{{asset('img/fv/factory_views_6_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_6.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_6')}}">
+                    <img src="{{asset('img/eq/eq_6_thum.jpg')}}" alt="{{Lang::get('page.eq_6')}}">
                 </a>
             </div>
-
         </div>
         <div class="row">
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_7.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 7">
-                    <img src="{{asset('img/fv/factory_views_7_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_7.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_7')}}">
+                    <img src="{{asset('img/eq/eq_7_thum.jpg')}}" alt="{{Lang::get('page.eq_7')}}">
                 </a>
             </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_8.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 8">
-                    <img src="{{asset('img/fv/factory_views_8_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_8.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_8')}}">
+                    <img src="{{asset('img/eq/eq_8_thum.jpg')}}" alt="{{Lang::get('page.eq_8')}}">
                 </a>
             </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_9.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 9">
-                    <img src="{{asset('img/fv/factory_views_9_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_9.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_9')}}">
+                    <img src="{{asset('img/eq/eq_9_thum.jpg')}}" alt="{{Lang::get('page.eq_9')}}">
                 </a>
             </div>
-
         </div>
         <div class="row">
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_10.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 10">
-                    <img src="{{asset('img/fv/factory_views_10_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_10.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_10')}}">
+                    <img src="{{asset('img/eq/eq_10_thum.jpg')}}" alt="{{Lang::get('page.eq_10')}}">
                 </a>
             </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_11.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 11">
-                    <img src="{{asset('img/fv/factory_views_11_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_11.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_11')}}">
+                    <img src="{{asset('img/eq/eq_11_thum.jpg')}}" alt="{{Lang::get('page.eq_11')}}">
                 </a>
             </div>
             <div class="col-xs-6 col-md-4">
-                <a href="{{asset('img/fv/factory_views_12.jpg')}}" class="thumbnail" data-lightbox="image-1"
-                   data-title="{{Lang::get('page.productionEquipment')}} 12">
-                    <img src="{{asset('img/fv/factory_views_12_thum.jpg')}}" alt="{{Lang::get('page.productionEquipment')}}">
+                <a href="{{asset('img/eq/eq_12.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_12')}}">
+                    <img src="{{asset('img/eq/eq_12_thum.jpg')}}" alt="{{Lang::get('page.eq_12')}}">
                 </a>
             </div>
-
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_13.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_13')}}">
+                    <img src="{{asset('img/eq/eq_13_thum.jpg')}}" alt="{{Lang::get('page.eq_13')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_14.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_14')}}">
+                    <img src="{{asset('img/eq/eq_14_thum.jpg')}}" alt="{{Lang::get('page.eq_14')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_15.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_15')}}">
+                    <img src="{{asset('img/eq/eq_15_thum.jpg')}}" alt="{{Lang::get('page.eq_15')}}">
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_16.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_16')}}">
+                    <img src="{{asset('img/eq/eq_16_thum.jpg')}}" alt="{{Lang::get('page.eq_16')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_17.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_17')}}">
+                    <img src="{{asset('img/eq/eq_17_thum.jpg')}}" alt="{{Lang::get('page.eq_17')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_18.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_18')}}">
+                    <img src="{{asset('img/eq/eq_18_thum.jpg')}}" alt="{{Lang::get('page.eq_18')}}">
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_19.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_19')}}">
+                    <img src="{{asset('img/eq/eq_19_thum.jpg')}}" alt="{{Lang::get('page.eq_19')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_20.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_20')}}">
+                    <img src="{{asset('img/eq/eq_20_thum.jpg')}}" alt="{{Lang::get('page.eq_20')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_21.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_21')}}">
+                    <img src="{{asset('img/eq/eq_21_thum.jpg')}}" alt="{{Lang::get('page.eq_21')}}">
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_22.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_22')}}">
+                    <img src="{{asset('img/eq/eq_22_thum.jpg')}}" alt="{{Lang::get('page.eq_22')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_23.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_23')}}">
+                    <img src="{{asset('img/eq/eq_23_thum.jpg')}}" alt="{{Lang::get('page.eq_23')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_24.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_24')}}">
+                    <img src="{{asset('img/eq/eq_24_thum.jpg')}}" alt="{{Lang::get('page.eq_24')}}">
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_25.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_25')}}">
+                    <img src="{{asset('img/eq/eq_25_thum.jpg')}}" alt="{{Lang::get('page.eq_25')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_26.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_26')}}">
+                    <img src="{{asset('img/eq/eq_26_thum.jpg')}}" alt="{{Lang::get('page.eq_26')}}">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <a href="{{asset('img/eq/eq_27.jpg')}}" class="thumbnail" data-lightbox="image-1"
+                   data-title="{{Lang::get('page.eq_27')}}">
+                    <img src="{{asset('img/eq/eq_27_thum.jpg')}}" alt="{{Lang::get('page.eq_27')}}">
+                </a>
+            </div>
         </div>
         <hr/>
     </div>
